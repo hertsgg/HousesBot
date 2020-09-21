14502420191905fd0ba23c7e91a1662bf07f2716
Stefan Cooper <stefan.cooper27@gmail.com>
2020-09-21 08:58:59 +0100

    Updated README and removed everything house related

--- README.md
+++ README.md
@@ -8,29 +8,36 @@ to check how many points their house has.
 
 ## Using the bot on discord
 
-### Current implemented commands (29/07/19)
+### Current implemented commands (21/09/2020)
 
-`!verify @NAME [nohouse]` to verify members (use the argument `nohouse` if you specifically don't want this member to be set a house) <br />
-`!add HOUSE SCORETOBEADDED` to add points to a house <br />
-`!leaderboard` to check the leaderboard of the houses <br />
+`!verify @NAME to verify members ` <br />
 `!ping` to check server latency to the bot <br />
-`!purge [verified][houses]` to remove all verified members or all members of houses <br />
-`!shuffle` to shuffle all members into randomly assigned houses <br />
+`!purge [verified]` to remove all verified members <br />
 `!alumni @NAME` to give a member the Society Alumni role <br />
 `!addstreamer @NAME @TWITCHID` to add a streamer to our database to be tracked for stats <br />
 `!checkmypoints` to check your current points earned as a streamer <br />
 
-## Setting up config.json
+## Setting up .env
 
 To set up this file you'll need to do a couple things.
 
 Firstly, you'll need to create a bot online using discords application process,
 once this is done copy and paste the App Token into the "token" section in the config.
 
-For the second half of the config you'll need to create some roles on discord for 
-holding scores and a "committee" role which should be used as an admin role. Make these
+For the second half of the config you'll need to create some roles and channels on discord for
+the varying IDs needed including a "committee" role which should be used as an admin role. Make these
 mentionable in the role permissions. Then get the role IDs by doing `\@ROLE` then copy the 
-numbers specifically into the config.json.
+numbers specifically into the .env.
+
+They should be organised like this in your `.env`
+
+token=BLAHBLAHBLAH
+prefix=!
+committeeID=123456
+serverID=123456
+mongoAddress=BLAHBLAHurlBLAH
+streamDiscord=123456
+twitchClientId=123456
 
 
 ## Setting up node/dependencies for the bot
--- bot.js
+++ bot.js
@@ -46,65 +46,8 @@ bot.on("message", async message => {
             m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
             break;
 
-        // // Add scores to houses
-        // case "add":
-        //     // ensure committee are the only people to add scores to houses
-        //     if (message.member.roles.has(process.env.committeeID)) {
-        //         let house = args[0];
-        //         let newScoreString = args[1];
-        //         let scoreInt = 0;
-        //         let scoreString = ''; 
-        //         if (house.toUpperCase() === "SLYTHERIN" ) {
-        //         scoreString = message.guild.roles.get(process.env.slytherinID);
-        //         scoreInt = parseInt(scoreString.name);
-        //         }
-        //         else if (house.toUpperCase() === "SSB" ) {
-        //         scoreString = message.guild.roles.get(process.env.ssbID);
-        //         scoreInt = parseInt(scoreString.name);
-        //         }
-        //         else if (house.toUpperCase() === "STEFCYKA" ) {
-        //         scoreString = message.guild.roles.get(process.env.stefcykaID); 
-        //         scoreInt = parseInt(scoreString.name);
-        //         }
-        //         else if (house.toUpperCase() === "DANNISTER" ) {
-        //         scoreString = message.guild.roles.get(process.env.dannisterID); 
-        //         scoreInt = parseInt(scoreString.name);
-        //         }
-        //         else {
-        //         message.reply(`the house ${house} doesn't exist`);
-        //         break;
-        //         }
-                
-        //         let newScoreInt = parseInt(newScoreString);
-        //         newScoreInt += scoreInt;
-        //         let newScore = newScoreInt.toString();
-        //         scoreString.edit({name: newScore});
-        //         message.reply(`added score to ${house}. New score: ${newScoreInt} Old score: ${scoreString.name}`);
-        //         break;
-        //     }
-        //     else {
-        //     message.reply(`Sorry you don't have permission to add scores to houses`);
-        //     break;
-        //     }
-
-        // // Leaderboard command to see where each house stands
-        // case "leaderboard":
-        //     dannisterScore = message.guild.roles.get(process.env.dannisterID); 
-        //     stefcykaScore= message.guild.roles.get(process.env.stefcykaID);
-        //     ssbScore = message.guild.roles.get(process.env.ssbID);
-        //     slytherinScore = message.guild.roles.get(process.env.slytherinID); 
-        //     message.channel.send(`\n **Slytherin**: *${slytherinScore.name}* \n**Team StefCyka**: *${stefcykaScore.name}* \n**House Dannister** *${dannisterScore.name}* \n**SSB Clan** *${ssbScore.name}*`);
-        //     break;
-
         // Verify members/Assign them their house
         case "verify":
-            let arg = args[1];
-            let slytherinID = message.guild.roles.find(role => role.name === "Slytherin");
-            let stefcykaID = message.guild.roles.find(role => role.name === "Team StefCyka");
-            let ssbID = message.guild.roles.find(role => role.name === "SSB Clan");
-            let dannisterID = message.guild.roles.find(role => role.name === "House Dannister");
-            let sortingChoice = Math.floor(Math.random()*(4-1+1)+1);
-            let sortingHat = [0,slytherinID,stefcykaID,ssbID,dannisterID];
             if (message.member.roles.has(process.env.committeeID)) {
                 let mem = message.mentions.members.first();
                 let verifiedRole = message.guild.roles.find(role => role.name === "✔️ Verified Member");
@@ -113,19 +56,14 @@ bot.on("message", async message => {
                 }
                 else {
                     mem.addRole(verifiedRole.id).catch(console.error);
-                    if (arg === "nohouse") {
-                        message.channel.send(`Congratulations ${mem}! You're now verified!`);
-                        message.delete(1000);
-                        break;
-                    }
-                    mem.addRole(sortingHat[sortingChoice]);
-                    message.channel.send(`Congratulations ${mem}! You're now verified! The sorting hat has selected ${sortingHat[sortingChoice]} as your new house!`);
+                    message.channel.send(`Congratulations ${mem}! You're now verified!`);
                     message.delete(1000);
+                    break;
                 }
                 break;
             }
             else {
-                message.reply(`Sorry you don't have permission to verify members`);
+                message.reply(`Sorry, you don't have permission to verify members`);
                 break;
             }
         
@@ -149,78 +87,16 @@ bot.on("message", async message => {
                                            Type '!purge verified confirm' if you're sure!`);
                         }
                         break;
-                    // case "houses":
-                    //     if (confirm === "confirm") {
-                    //         let slytherinID = message.guild.roles.find(role => role.name === "Slytherin");
-                    //         let stefcykaID = message.guild.roles.find(role => role.name === "Team StefCyka");
-                    //         let ssbID = message.guild.roles.find(role => role.name === "SSB Clan");
-                    //         let dannisterID = message.guild.roles.find(role => role.name === "House Dannister");
-                    //         list.members.forEach(member => {
-                    //             if (member.roles.has(slytherinID.id)) member.removeRole(slytherinID)
-                    //             else if (member.roles.has(stefcykaID.id)) member.removeRole(stefcykaID)
-                    //             else if (member.roles.has(ssbID.id)) member.removeRole(ssbID)
-                    //             else if (member.roles.has(dannisterID.id)) member.removeRole(dannisterID)
-                    //         });
-                    //         message.reply("All house members roles' removed");
-                    //     }
-                    //     else {
-                    //         message.reply(`Woah are you sure you want to do this? This will remove all the houses roles on the server. 
-                    //                        Type '!purge houses confirm' if you're sure!`);
-                    //     }
-                        break;
                     default:
-                        message.reply("You can only purge the following types: `houses` or `verified`");
+                        message.reply("You can only purge the following types: `verified`");
                         break;
                 };
                 break;
             }
             else {
-                message.reply(`Sorry you don't have permission to purge members' roles `);
+                message.reply(`Sorry, you don't have permission to purge members' roles `);
                 break;
             }
-
-        // // Shuffle houses
-        // case "shuffle":
-        //     if (message.member.roles.has(process.env.committeeID)) {
-        //         let confirm = args[0];
-        //         const msg = await message.channel.send("Checking "+list.members.size+" users...");
-        //         if (confirm === "confirm") {
-        //             let membersShuffled = 1;
-        //             let rolesRemoved = 0;
-        //             const m = await message.channel.send("Sorting verified members...");
-        //             let verifiedRole = message.guild.roles.find(role => role.name === "✔️ Verified Member");
-        //             let slytherinID = message.guild.roles.find(role => role.name === "Slytherin");
-        //             let stefcykaID = message.guild.roles.find(role => role.name === "Team StefCyka");
-        //             let ssbID = message.guild.roles.find(role => role.name === "SSB Clan");
-        //             let dannisterID = message.guild.roles.find(role => role.name === "House Dannister");
-        //             list.members.forEach(member => {
-        //                 if (member.roles.has(verifiedRole.id)) {
-        //                     if (member.roles.has(slytherinID.id)) member.removeRole(slytherinID)
-        //                     else if (member.roles.has(stefcykaID.id)) member.removeRole(stefcykaID)
-        //                     else if (member.roles.has(ssbID.id)) member.removeRole(ssbID)
-        //                     else if (member.roles.has(dannisterID.id)) member.removeRole(dannisterID)
-        //                     rolesRemoved++;
-        //                     m.edit(`${message.author} Roles removed: ${rolesRemoved} Members Shuffled: ${membersShuffled-1}`);
-        //                     setTimeout(function(){
-        //                         let sortingChoice = Math.floor(Math.random()*(4-1+1)+1);
-        //                         let sortingHat = [0,slytherinID,stefcykaID,ssbID,dannisterID];
-        //                         add(member,sortingHat[sortingChoice])
-        //                         m.edit(`${message.author} Roles removed: ${rolesRemoved} Members Shuffled: ${membersShuffled-1}`);
-        //                         membersShuffled++;
-                                
-        //                     }, 5000);
-        //                 }
-        //             });
-        //             break;
-        //         }
-        //         else {
-        //             message.reply(`Woah are you sure you want to do this? This will shuffle everyones houses in the server. Type '!shuffle confirm' if you're sure!`);
-        //         }
-        //     }
-        //     else {
-        //         message.reply(`Sorry, you don't have permission to shuffle members`);
-        //         break;
-        //     }
         
         case "alumni":
             if (message.member.roles.has(process.env.committeeID)) {
@@ -257,21 +133,16 @@ bot.on("message", async message => {
                 message.reply(`Sorry, you don't have permission to add new streamers`)
                 break;
             }
+
         case "checkmypoints":
             const mess = await message.channel.send("Checking points...");
             await checkMyPoints(message.author.id, mess);
             break;
 
         default:
-            message.channel.send(`Sorry, I don't quite understand what you're asking. You can find more information about me here: \n\n https://github.com/hertsgg/HousesBot`);
-            
+            message.channel.send(`Sorry, I don't quite understand what you're asking. You can find more information about me here: \n\n https://github.com/hertsgg/HousesBot`); 
     }
-
    });
-
-async function add(member,choice) {
-    await member.addRole(choice).catch(console.error);
-}
 
 async function checkMyPoints(userId, m) {
     await MongoClient.connect(process.env.mongoAddress, {useNewUrlParser: true}, (err, client) => {
